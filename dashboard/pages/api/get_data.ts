52e788eabfc00187d179e83a7269efef5934af6d
sdairs <sdairs@users.noreply.github.com>
2023-01-28 20:07:02 +0000

    test fetches

--- dashboard/pages/api/get_data.ts
+++ dashboard/pages/api/get_data.ts
@@ -14,22 +14,34 @@ async function handler(
     const first = now.subtract(30, 'minute').format('YYYY-MM-DDTHH:mm[Z]');
     const second = now.subtract(60, 'minute').format('YYYY-MM-DDTHH:mm[Z]');
 
-    const intensity_first = await fetch(intensity_url + first);
-    const intensity_second = await fetch(intensity_url + second);
-    const generation_first = await fetch(generation_url + first + '/' + now.format('YYYY-MM-DDTHH:mm[Z]'));
-    const generation_second = await fetch(generation_url + second + '/' + first);
-    console.log(intensity_url + first);
-    console.log(intensity_url + second);
-    console.log(generation_url + first + '/' + now.format('YYYY-MM-DDTHH:mm[Z]'));
-    console.log(generation_url + second + '/' + first);
-    console.log(intensity_first);
-    console.log(intensity_second);
-    console.log(generation_first);
-    console.log(generation_second);
+    await fetch(intensity_url + first)
+        .then((response) => response.json())
+        .then((data) => sendToTinybird(data['data']));
+    await fetch(intensity_url + second)
+        .then((response) => response.json())
+        .then((data) => sendToTinybird(data['data']));
+    await fetch(generation_url + first + '/' + now.format('YYYY-MM-DDTHH:mm[Z]'))
+        .then((response) => response.json())
+        .then((data) => sendToTinybird(data['data']));
+    await fetch(generation_url + second + '/' + first)
+        .then((response) => response.json())
+        .then((data) => sendToTinybird(data['data']));
+    // console.log(intensity_url + first);
+    // console.log(intensity_url + second);
+    // console.log(generation_url + first + '/' + now.format('YYYY-MM-DDTHH:mm[Z]'));
+    // console.log(generation_url + second + '/' + first);
+    // console.log(intensity_first);
+    // console.log(intensity_second);
+    // console.log(generation_first);
+    // console.log(generation_second);
 
     res.status(200).end();
 }
 
+
+async function sendToTinybird(data: any) {
+    console.log(data);
+}
 
 export default verifySignature(handler);
 
