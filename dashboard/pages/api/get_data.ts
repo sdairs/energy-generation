5f7c5a35c39576c752c4de5c79dc74a89fa1dfb1
sdairs <sdairs@users.noreply.github.com>
2023-01-28 22:58:34 +0000

    transforms & send to tb

--- dashboard/pages/api/get_data.ts
+++ dashboard/pages/api/get_data.ts
@@ -5,6 +5,25 @@ import dayjs from 'dayjs';
 const generation_url = 'https://api.carbonintensity.org.uk/generation/';
 const intensity_url = 'https://api.carbonintensity.org.uk/intensity/';
 
+// type intensityData = {
+//     from: string,
+//     to: string,
+//     intensity: {
+//         forecast: number,
+//         actual: number,
+//         index: string
+//     }
+// }
+
+// type generationData = {
+//     from: string,
+//     to: string,
+//     intensity: {
+//         forecast: number,
+//         actual: number,
+//         index: string
+//     }
+// }
 
 async function handler(
     req: NextApiRequest,
@@ -16,31 +35,52 @@ async function handler(
 
     await fetch(intensity_url + first)
         .then((response) => response.json())
-        .then((data) => sendToTinybird(data['data']));
+        .then((data) => sendToTinybird(intensityTransform(data['data'][0]), 'intensity'));
     await fetch(intensity_url + second)
         .then((response) => response.json())
-        .then((data) => sendToTinybird(data['data']));
+        .then((data) => sendToTinybird(intensityTransform(data['data'][0]), 'intensity'));
     await fetch(generation_url + first + '/' + now.format('YYYY-MM-DDTHH:mm[Z]'))
         .then((response) => response.json())
-        .then((data) => sendToTinybird(data['data']));
+        .then((data) => sendToTinybird(generationTransform(data['data'][0]), 'generation'));
     await fetch(generation_url + second + '/' + first)
         .then((response) => response.json())
-        .then((data) => sendToTinybird(data['data']));
-    // console.log(intensity_url + first);
-    // console.log(intensity_url + second);
-    // console.log(generation_url + first + '/' + now.format('YYYY-MM-DDTHH:mm[Z]'));
-    // console.log(generation_url + second + '/' + first);
-    // console.log(intensity_first);
-    // console.log(intensity_second);
-    // console.log(generation_first);
-    // console.log(generation_second);
+        .then((data) => sendToTinybird(generationTransform(data['data'][0]), 'generation'));
 
     res.status(200).end();
 }
 
+function intensityTransform(data: any) {
+    return {
+        time_from: data.from,
+        time_to: data.to,
+        intensity_actual: data.intensity.actual,
+        intensity_forecast: data.intensity.forecast,
+        intensity_index: data.intensity.index,
+    };
+}
 
-async function sendToTinybird(data: any) {
+function generationTransform(data: any) {
+    let transform = {
+        time_from: data.from,
+        time_to: data.to,
+    }
+    data.generationmix.forEach((item: any) => {
+        transform[item.fuel] = item.perc;
+    })
+    return transform;
+}
+
+
+async function sendToTinybird(data: any, datasource: string) {
     console.log(data);
+    fetch(
+        `https://api.tinybird.co/v0/events?name=${datasource}`,
+        {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: { Authorization: `Bearer ${process.env.TINYBIRD_TOKEN}` }
+        }
+    );
 }
 
 export default verifySignature(handler);
