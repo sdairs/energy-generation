962b4256e7562c2c64ed9a38916088f2c1399050
sdairs <sdairs@users.noreply.github.com>
2023-01-28 19:38:46 +0000

    test hit generation api

--- dashboard/pages/api/get_data.ts
+++ dashboard/pages/api/get_data.ts
@@ -1,5 +1,9 @@
 import { verifySignature } from "@upstash/qstash/nextjs";
 import { NextApiRequest, NextApiResponse } from 'next';
+import dayjs from 'dayjs';
+
+const generation_url = 'https://api.carbonintensity.org.uk/generation/';
+const intensity_url = 'https://api.carbonintensity.org.uk/intensity/';
 
 
 async function handler(
@@ -7,6 +11,11 @@ async function handler(
     res: NextApiResponse
 ) {
     console.log("If this is printed, the signature has already been verified");
+    const now = dayjs();
+    const first = now.subtract(30, 'minute').format('YYYY-MM-DDTHH:mm[Z]');
+    const second = now.subtract(60, 'minute').format('YYYY-MM-DDTHH:mm[Z]');
+
+    console.log(fetch(generation_url + first));
 
     res.status(200).end();
 }
