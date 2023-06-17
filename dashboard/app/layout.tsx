e32fabc42abf432a06711209db9a278d83d87cbf
sdairs <sdairs@users.noreply.github.com>
2023-06-17 20:40:46 +0100

    use head

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Head from "./head";
 
 export default function RootLayout({
   children,
@@ -7,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head />
+      <Head></Head>
       <body>{children}</body>
     </html>
   );
